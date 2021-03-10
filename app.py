eb540ff86812d98e5e65fb286a46c42c45b25664
naikshubham14 <shubhamnaik321@gmail.com>
2021-03-11 00:13:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ crop_recommendation_model_path = 'models/RandomForest.pkl'
 crop_recommendation_model = pickle.load(
     open(crop_recommendation_model_path, 'rb'))
 
-crop_damge_model = pickle.load(open(r"C:\Users\shubh\OneDrive\Documents\Major Project\models\Crop Damge Models\CD_DecisionTree.pkl", 'rb'))
+crop_damge_model = pickle.load(open('models/CD_DecisionTree.pkl", 'rb'))
 
 # =========================================================================================
 
