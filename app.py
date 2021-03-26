04dc68c46c48c6b285ba46d4b2706b23ad46e8e3
naikshubham14 <shubhamnaik321@gmail.com>
2021-03-26 10:20:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,197 @@
+from flask import Flask, render_template, request, Markup
+import numpy as np
+import pandas as pd
+from utils.fertilizer import fertilizer_dic
+import requests
+import config
+import pickle
+# ==============================================================================================
+
+# -------------------------LOADING THE TRAINED MODELS -----------------------------------------------
+
+# Loading crop recommendation model
+
+crop_recommendation_model_path = 'models/RandomForest.pkl'
+crop_recommendation_model = pickle.load(open(crop_recommendation_model_path, 'rb'))
+
+crop_damge_model = pickle.load(open(r"C:\Users\shubh\OneDrive\Documents\Major Project\app\models\CD_DecisionTree.pkl", 'rb'))
+
+# =========================================================================================
+
+# Custom functions for calculations
+
+
+def weather_fetch(city_name):
+    """
+    Fetch and returns the temperature and humidity of a city
+    :params: city_name
+    :return: temperature, humidity
+    """
+    api_key = config.weather_api_key
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+
+    if x["cod"] != "404":
+        y = x["main"]
+
+        temperature = round((y["temp"] - 273.15), 2)
+        humidity = y["humidity"]
+        return temperature, humidity
+    else:
+        return None
+
+# ===============================================================================================
+# ------------------------------------ FLASK APP -------------------------------------------------
+
+
+app = Flask(__name__)
+
+# render home page
+
+
+@ app.route('/')
+def home():
+    title = 'AgriSmart - Home'
+    return render_template('index.html', title=title)
+
+# render crop recommendation form page
+
+
+@ app.route('/crop-recommend')
+def crop_recommend():
+    title = 'AgriSmart - Crop Recommendation'
+    return render_template('crop.html', title=title)
+
+# render fertilizer recommendation form page
+
+
+@ app.route('/fertilizer')
+def fertilizer_recommendation():
+    title = 'AgriSmart - Fertilizer Suggestion'
+
+    return render_template('fertilizer.html', title=title)
+
+# render damage prediction input page
+
+@ app.route('/crop-damage')
+def crop_damage():
+    title = 'AgriSmart - Crop Health Prediction'
+
+    return render_template('crop-damage.html', title=title)
+
+#@ app.route('/team')
+#def team():
+ #   title = 'AgriSmart - Team'
+
+  #  return render_template('team.html', title=title)
+
+
+
+
+
+# ===============================================================================================
+
+# RENDER PREDICTION PAGES
+
+# render crop recommendation result page
+
+
+@ app.route('/crop-predict', methods=['POST'])
+def crop_prediction():
+    title = 'Harvestify - Crop Recommendation'
+
+    if request.method == 'POST':
+        N = int(request.form['nitrogen'])
+        P = int(request.form['phosphorous'])
+        K = int(request.form['pottasium'])
+        ph = float(request.form['ph'])
+        rainfall = float(request.form['rainfall'])
+
+        # state = request.form.get("stt")
+        city = request.form.get("city")
+
+        if weather_fetch(city) != None:
+            temperature, humidity = weather_fetch(city)
+            data = np.array([[N, P, K, temperature, humidity, ph, rainfall]])
+            my_prediction = crop_recommendation_model.predict(data)
+            final_prediction = my_prediction[0]
+
+            return render_template('crop-result.html', prediction=final_prediction, title=title)
+
+        else:
+
+            return render_template('try_again.html', title=title)
+
+# render fertilizer recommendation result page
+
+
+@ app.route('/fertilizer-predict', methods=['POST'])
+def fert_recommend():
+    title = 'Harvestify - Fertilizer Suggestion'
+
+    crop_name = str(request.form['cropname'])
+    N = int(request.form['nitrogen'])
+    P = int(request.form['phosphorous'])
+    K = int(request.form['pottasium'])
+    # ph = float(request.form['ph'])
+
+    df = pd.read_csv('Data/fertilizer.csv')
+
+    nr = df[df['Crop'] == crop_name]['N'].iloc[0]
+    pr = df[df['Crop'] == crop_name]['P'].iloc[0]
+    kr = df[df['Crop'] == crop_name]['K'].iloc[0]
+
+    n = nr - N
+    p = pr - P
+    k = kr - K
+    temp = {abs(n): "N", abs(p): "P", abs(k): "K"}
+    max_value = temp[max(temp.keys())]
+    if max_value == "N":
+        if n < 0:
+            key = 'NHigh'
+        else:
+            key = "Nlow"
+    elif max_value == "P":
+        if p < 0:
+            key = 'PHigh'
+        else:
+            key = "Plow"
+    else:
+        if k < 0:
+            key = 'KHigh'
+        else:
+            key = "Klow"
+
+    response = Markup(str(fertilizer_dic[key]))
+
+    return render_template('fertilizer-result.html', recommendation=response, title=title)
+
+#Crop Damage Prediction render
+@app.route('/predict', methods=['POST'])
+def dam_pred():
+    data1= request.form['Estimated_Insects_Count']
+    data2= request.form['Crop_Type']
+    data3= request.form['Soil_Type']
+    data4= request.form['Pesticide_Use_Category']
+    data5= request.form['Number_Doses_Week']
+    data6= request.form['Number_Weeks_Used']
+    data6 = float(data6)
+    data7= request.form['Number_Weeks_Quit']
+    data8= request.form['Season']
+    arr = np.array([[data1,data2,data3,data4,data5,data6,data7,data8]])
+    pred_c = crop_damge_model.predict(arr)
+    if pred_c==0:
+        prediction="Healthy"
+    elif pred_c==1:
+        prediction="Prone to Damage"
+    else:
+        prediction="Damaged"
+    
+    return render_template('crop-damage-result.html', data=prediction)
+
+# ===============================================================================================
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
